a568d168e5cdc1d484377f8f95246889dea43871
erez <erzez17@gmail.com>
2020-02-07 02:51:33 -0800

    adding file

---
+++ api.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, json, request
+
+pl = [
+    {
+        "song": "R U MINE?",
+        "length": "3:04",
+        "artist": "Arctic Monkeys"
+    },
+     {
+        "song": "Dance, Dance",
+        "length": "3:04",
+        "artist": "Fall Out Boys"
+i    },
+    {
+        "song": "redbone",
+        "length": "4:37",
+        "artist": "Childish Gambino"
+    }
+]
+
+api = Flask(__name__)
+
+
+
+@api.route('/func', methods=['GET'])
+def get():
+    a = jsonify(pl)
+    return a
+
+
+@api.route('/func', methods=['PUT'])
+def put():
+    body = request.json
+    pl.insert(0, body)
+    a = jsonify(pl)
+    return a
+
+
+@api.route('/func', methods=['DELETE'])
+def delete():
+    body = request.json
+    for i in range(0, len(pl)-1):
+        b = pl[i]
+        if body["song"] == b["song"]:
+            pl.remove(pl[i])
+    a = jsonify(pl)
+    return a
+
+
+if __name__ == "__main__":
+    api.run(debug=True, port=80, host="0.0.0.0")
+
